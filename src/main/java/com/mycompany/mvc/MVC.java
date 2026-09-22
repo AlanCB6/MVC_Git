dfd9e7838a2901dfa2c278e8b4e4c80a94e9c5a1
Alan Chan <johndoe@example.com>
2026-09-21 19:59:31 -0600

    Ultima

--- src/main/java/com/mycompany/mvc/MVC.java
+++ src/main/java/com/mycompany/mvc/MVC.java
@@ -1,16 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.mvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- *
- * @author Alan Chan
- */
 @SpringBootApplication
 public class MVC {
 
